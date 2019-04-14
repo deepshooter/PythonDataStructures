834d2f8075d2b7fc60e032a0cc0dd8e8f9a3a6ea
deepshooter <avinashm720@gmail.com>
2019-04-14 11:36:13 +0530

    Adding new programs

---
+++ Strings/strings_function.py
@@ -0,0 +1,10 @@
+greet = 'Hello Bob'
+zap = greet.lower()
+print(zap)
+print(greet)
+print('Hi There'.lower())
+print('....................')
+
+stuff = 'Hello World'
+print(type(stuff))
+print(dir(stuff))
